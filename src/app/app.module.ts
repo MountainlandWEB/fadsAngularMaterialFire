d528d62a01ad023d115633e902cb7a4a44edc696
Adrien Donat <I23332@xactware.com>
2018-06-04 10:22:47 -0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { HomeComponent } from './home/home.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import {MatButtonModule, MatSidenavModule, MatTabsModule} from '@angular/material';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    HomeComponent,
+    UserProfileComponent,
+    ProductsComponent,
+    LogoutComponent,
+    ContactUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatTabsModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'profile', component: UserProfileComponent },
+      { path: 'contact', component: ContactUsComponent },
+      { path: 'logout', component: LogoutComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
